58eb3b0e0e411c74eefa94334e96a98c32087efd
oxdev03 <140103378+oxdev03@users.noreply.github.com>
2024-05-25 19:54:04 +0200

    test value function

--- src/filter/test-value.ts
+++ src/filter/test-value.ts
@@ -1,10 +1,19 @@
-type FlatType = string | number | undefined | boolean | bigint | null;
+type FlatType = string | number | undefined | boolean | null;
 
-export function testString(value: FlatType, filter: string, quoted: boolean): boolean {
+export function testString(value: FlatType, filter: string | number, quoted: boolean): boolean {
+  if (value == undefined || value == null) {
+    return false;
+  } else if (quoted) {
+    return String(value) == filter;
+  } else if (typeof filter == 'string') {
+    return String(value).includes(filter);
+  } else if (typeof filter == 'number' || typeof filter == 'boolean') {
     return value == filter;
+  }
+
+  throw new Error(`Test String ${value} with filter ${filter} not covered`);
 }
 
 export function testRegexp(value: FlatType, filter: string) {
-
-    return false;
+  return false;
 }
