30b637048eb76d948865431de5ab423d4e40659a
oxdev03 <140103378+oxdev03@users.noreply.github.com>
2024-06-01 19:39:59 +0200

    benchmark test queries

---
+++ src/benchmark/filter.bench.ts
@@ -0,0 +1,23 @@
+import { bench, describe } from 'vitest';
+import testFilterQueries from '../tests/__fixtures__/filter';
+import personData from '../tests/__fixtures__/data-person';
+import { QueryParser, filter } from '..';
+import ReferenceResolver from '../handlers/resolver';
+
+const testGroups = new Set(testFilterQueries.map((t) => t.group));
+
+describe('Filter', () => {
+  testGroups.forEach((group) => {
+    const testQuery = testFilterQueries.filter((t) => t.group == group);
+    // prepare ast, so its not considered in benchmark
+    const queries = testQuery.map((t) => {
+      const ast = new QueryParser(t.query).parse();
+      return () => filter(ast, personData, new ReferenceResolver(t.variableResolver || {}, t.functionResolver));
+    });
+    bench(`should filter with ${group}`, () => {
+      queries.forEach((filterData) => {
+        filterData();
+      });
+    });
+  });
+});
