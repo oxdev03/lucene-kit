444e081610b851832cecd3df071fd4a05453ece8
oxdev03 <140103378+oxdev03@users.noreply.github.com>
2025-10-24 22:18:49 +0200

    chore(deps-dev): update several devDependencies to latest versions

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import codspeedPlugin from '@codspeed/vitest-plugin';
-
 export default defineConfig({
   plugins: [codspeedPlugin()],
   test: {
+    include: ['src/**/*.test.ts'],
     coverage: {
       exclude: ['scripts/**', 'src/benchmark/**', 'src/xlucene/lucene.ts', 'commitlint.config.js'],
     },
